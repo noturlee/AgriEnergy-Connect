ba181ec1fcbf580ab46d35e5c138f19791c0bdee
Leighche <ST10033808@vcconnect.edu.za>
2024-05-13 04:14:16 +0200

    Version 1
    Version 1 of Agri-Energy Connect has no UI, but necessary functionality, Session or some sort of Role-based authorisation still needs to be implemented.

---
+++ FireBasics/FireBasics/Controllers/CheckOutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FireBasics.Controllers
+{
+    public class CheckOutController : Controller
+    {
+        public IActionResult Pay()
+        {
+            return View();
+        }
+    }
+}
