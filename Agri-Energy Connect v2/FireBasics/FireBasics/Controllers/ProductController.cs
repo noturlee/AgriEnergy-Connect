d5f9e5ed85e75a26ec4c4478385d3d5ee7eff728
Leighche <ST10033808@vcconnect.edu.za>
2024-05-14 00:21:30 +0200

    Version 2
    Some UI, Authentication , About, Payment still needs Designing, Role Based Auth still needs work

---
+++ Agri-Energy Connect v2/FireBasics/FireBasics/Controllers/ProductController.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FireBasics.Models;
+
+namespace FireBasics.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly AgrienergyContext _context;
+
+        public ProductController(AgrienergyContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Product
+        public async Task<IActionResult> Index()
+        {
+            var agrienergyContext = _context.Products.Include(p => p.Category).Include(p => p.Farmer);
+            return View(await agrienergyContext.ToListAsync());
+        }
+
+        // GET: Product/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var farmer = await _context.Users
+                .Include(u => u.Products)
+                .FirstOrDefaultAsync(u => u.UserId == id && u.Role == "FARMER");
+
+            if (farmer == null)
+            {
+                return NotFound();
+            }
+
+            return View(farmer);
+        }
+
+        public async Task<IActionResult> FarmerDetails(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var farmer = await _context.Users
+                .Include(u => u.Products)
+                .FirstOrDefaultAsync(u => u.UserId == id);
+
+            if (farmer == null)
+            {
+                return NotFound();
+            }
+
+            return View(farmer);
+        }
+
+        // GET: Product/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryName");
+            ViewData["FarmerId"] = new SelectList(_context.Users, "UserId", "Name");
+            return View();
+        }
+
+        // POST: Product/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ProductId,ProductName,ProductDescription,Price,Quantity,Availability,ProductionDate,FarmerId,CategoryId,Image_Url")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId", product.CategoryId);
+            ViewData["FarmerId"] = new SelectList(_context.Users, "UserId", "UserId", product.FarmerId);
+            return View(product);
+        }
+
+
+        // GET: Product/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId", product.CategoryId);
+            ViewData["FarmerId"] = new SelectList(_context.Users, "UserId", "UserId", product.FarmerId);
+            return View(product);
+        }
+
+        // POST: Product/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ProductId,ProductName,ProductDescription,Price,Quantity,Availability,ProductionDate,FarmerId,CategoryId,Image_Url")] Product product)
+        {
+            if (id != product.ProductId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(product);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductExists(product.ProductId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId", product.CategoryId);
+            ViewData["FarmerId"] = new SelectList(_context.Users, "UserId", "UserId", product.FarmerId);
+            return View(product);
+        }
+
+        // GET: Product/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products
+                .Include(p => p.Category)
+                .Include(p => p.Farmer)
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductExists(int id)
+        {
+            return _context.Products.Any(e => e.ProductId == id);
+        }
+
+
+
+    }
+}
---
+++ Agri-Energy Connect v2/FireBasics/FireBasics/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FireBasics.Models;
+
+public partial class Product
+{
+    public int ProductId { get; set; }
+
+    public string? ProductName { get; set; }
+
+    public string? ProductDescription { get; set; }
+
+    public double? Price { get; set; }
+
+    public int? Quantity { get; set; }
+
+    public string? Availability { get; set; }
+
+    public DateOnly? ProductionDate { get; set; }
+
+    public int? FarmerId { get; set; }
+
+    public int? CategoryId { get; set; }
+
+    public string Image_Url { get; set; }
+
+    public virtual Category? Category { get; set; }
+
+    public virtual User? Farmer { get; set; }
+}
