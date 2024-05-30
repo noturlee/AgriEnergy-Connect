08e6c73d804f715de6cab12098597099db7534d9
Leighche <ST10033808@vcconnect.edu.za>
2024-05-31 00:35:26 +0200

    Application v3

---
+++ FireBasics/FireBasics/Models/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FireBasics.Models;
+
+public partial class Product
+{
+    public int ProductId { get; set; }
+
+    public string? ProductName { get; set; }
+
+    public string? ProductDescription { get; set; }
+
+    public string? ImageUrl { get; set; }
+
+    public double? Price { get; set; }
+
+    public int? Quantity { get; set; }
+
+    public string? Availability { get; set; }
+
+    public DateOnly? ProductionDate { get; set; }
+
+    public int? FarmerId { get; set; }
+
+    public int? CategoryId { get; set; }
+
+    public virtual ICollection<Cart> Carts { get; set; } = new List<Cart>();
+
+    public virtual Category? Category { get; set; }
+
+    public virtual User? Farmer { get; set; }
+}
